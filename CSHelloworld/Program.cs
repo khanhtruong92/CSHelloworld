9567b11edab5f1e57664f5a00a49086bb48d63c0
khanhtruong92 <khanhtq@pascaliaasia.com>
2023-09-02 16:22:08 +0700

    Update Program.cs
    Comments homework

--- CSHelloworld/Program.cs
+++ CSHelloworld/Program.cs
@@ -2,6 +2,9 @@
 {
     internal class Program
     {
+        // This is a single-line comment
+        /* This is a multi-line comment */
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
