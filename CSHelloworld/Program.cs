92d7316f794b2f50a4661bd33716b4d813b51658
khanhtruong92 <khanhtq@pascaliaasia.com>
2023-09-02 22:39:00 +0700

    data types homework

--- CSHelloworld/Program.cs
+++ CSHelloworld/Program.cs
@@ -4,14 +4,13 @@
     {
         static void Main(string[] args)
         {
-            int myNum = 50;
-            int x = 5;
-            int y = 10;
-            int z = x + y;
-            Console.WriteLine(z);
-
-            int a = 5, b = 6, c = 50;
-            Console.WriteLine(a + b + c);
+            int myNum = 9;
+            double myDoubleNum = 8.99;
+            char myLetter = 'A';
+            bool myBoolean = false;
+            string myText = "Hello World";
+            int myInt = 10;
+            Console.WriteLine(Convert.ToString(myInt));
         }
     }
 }
