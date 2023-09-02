043c8acac30a1f5721469e958f9fe8eac09b290d
khanhtruong92 <khanhtq@pascaliaasia.com>
2023-09-02 22:46:24 +0700

    User Input homework

--- CSHelloworld/Program.cs
+++ CSHelloworld/Program.cs
@@ -4,13 +4,9 @@
     {
         static void Main(string[] args)
         {
-            int myNum = 9;
-            double myDoubleNum = 8.99;
-            char myLetter = 'A';
-            bool myBoolean = false;
-            string myText = "Hello World";
-            int myInt = 10;
-            Console.WriteLine(Convert.ToString(myInt));
+            Console.WriteLine("Enter username:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
         }
     }
 }
