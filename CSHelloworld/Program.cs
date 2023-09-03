9007131772a92b90705b7525a191185ec5d0ebe9
khanhtruong92 <khanhtq@pascaliaasia.com>
2023-09-03 23:35:32 +0700

    Short Hand If...Else (Ternary Operator) homework

--- CSHelloworld/Program.cs
+++ CSHelloworld/Program.cs
@@ -1,13 +1,32 @@
-﻿namespace CSHelloworld
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace CSHelloworld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            bool isCodingFun = true;
-            bool isFishTasty = false;
-            Console.WriteLine(isCodingFun);
-            Console.WriteLine(isFishTasty);
+            //Print "Hello World" if x is greater than y.
+            int x = 50;
+            int y = 10;
+            if (x > y)
+{
+                Console.WriteLine("Hello World");
+            }
+
+            //Print "Hello World" if x is equal to y.
+            int a = 50;
+            int b = 50;
+            if (a == b)
+{
+                Console.WriteLine("Hello World");
+            }
+
+            //Insert the missing parts to complete the following "short hand if...else statement"(ternary operator):
+            int time = 20;
+            string result = (time < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
         }
     }
 }
