4e227fb68b8282024c484d91bd918c464e836b09
khanhtruong92 <khanhtq@pascaliaasia.com>
2023-09-05 15:44:48 +0700

    homework 1

--- CSHelloworld/Program.cs
+++ CSHelloworld/Program.cs
@@ -1,33 +1,19 @@
-﻿using System.Net.WebSockets;
+﻿using System.ComponentModel;
+using System.Net.WebSockets;
 
 namespace CSHelloworld
 {
     internal class Program
     {
-        // This is a single-line comment
-        /* This is a multi-line comment */
-
-        static int PlusMethodInt(int x, int y)
+        //Create a method named MyMethod and call it inside Main().
+        static void MyMethod()
         {
-            return x + y;
+            Console.WriteLine("I just got executed!");
         }
-
-        static double PlusMethodDouble(double x, double y)
-        {
-            return x - y;
-        }
-
 
         static void Main(string[] args)
         {
-            //var test2 = new MyMethod();
-            //test2.Name = "Test";
-            //test2.test();
-            int myNum1 = PlusMethodInt(8, 5);
-            double myNum2 = PlusMethodDouble(4.3, 6.26);
-            Console.WriteLine("Int: " + myNum1);
-            Console.WriteLine("Double: " + myNum2);
-
+            MyMethod();
         }
     }
 }
