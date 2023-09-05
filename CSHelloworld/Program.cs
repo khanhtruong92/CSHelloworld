43497b672b05ccd6fe0165d6f425e78d83409740
khanhtruong92 <khanhtq@pascaliaasia.com>
2023-09-05 16:59:31 +0700

    homework 3

--- CSHelloworld/Program.cs
+++ CSHelloworld/Program.cs
@@ -6,14 +6,30 @@ namespace CSHelloworld
 {
     internal class Program
     {
-        // homework 2: ax + b = 0 Write a function to find x from Linear equation.
-        static double findX(double a, double b)
+        // homework 3: ax^2 + bx + c = 0; Write a function to find x from Quadratic equation
+        static void findX(double a, double b, double c)
         {
-            return -b / a;
+            double delta = (b * b) - 4 * a * c;
+            if (delta < 0) 
+            {
+                Console.WriteLine("Vo nghiem");
+            }
+            else if (delta == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine("Co 1 nghiem: " + x);
+            }
+            else 
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("Co 2 nghiem la: " + x1 + " va " + x2);
+            }
+            
         }
         static void Main(string[] args)
         {
-            Console.WriteLine(findX(2, 3)); // -1.5: because 2x + 3 = 0 => x = -1.5;
+            findX(1, -4, 4);
         }
 
     }
