23c42f7f7ab19e3127ca1f37b6ef8b18d4299e14
khanhtruong92 <khanhtq@pascaliaasia.com>
2023-09-05 15:20:10 +0700

    new comment

--- CSHelloworld/Program.cs
+++ CSHelloworld/Program.cs
@@ -1,13 +1,33 @@
-﻿namespace CSHelloworld
+﻿using System.Net.WebSockets;
+
+namespace CSHelloworld
 {
     internal class Program
     {
         // This is a single-line comment
         /* This is a multi-line comment */
 
+        static int PlusMethodInt(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double PlusMethodDouble(double x, double y)
+        {
+            return x - y;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var test2 = new MyMethod();
+            //test2.Name = "Test";
+            //test2.test();
+            int myNum1 = PlusMethodInt(8, 5);
+            double myNum2 = PlusMethodDouble(4.3, 6.26);
+            Console.WriteLine("Int: " + myNum1);
+            Console.WriteLine("Double: " + myNum2);
+
         }
     }
 }
